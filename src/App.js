596817e085e1353f35b231a98bb3dac544503973
mmmmm1233212344 <abhaymilan8@gmail.com>
2022-06-10 08:32:49 +0530

    Finished Creating And Designing Of Banner Section With Css On React Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './Components/NavBar/NavBar.js';
 import './App.css'
+import Banner from './Components/Banner/Banner.js';
 
 
 function App() {
   return (
     <div className="App">
     <NavBar/>
+    <Banner/>
     </div>
   );
 }
