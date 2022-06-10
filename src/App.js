7241c056ea4e3bf9950bde3e1a2bc0c7d97ad6f6
mmmmm1233212344 <abhaymilan8@gmail.com>
2022-06-10 07:40:50 +0530

    Started Project React Netflix Clone App And Completed Styling Of The Navbar And Also The Logo And Avatar Of EThe Project React Clone App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar.js';
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="App">
+    <NavBar/>
+    </div>
+  );
+}
+
+export default App;
