9eed341c40be10ed0108e54cc6c0106eda9c56fe
mmmmm1233212344 <abhaymilan8@gmail.com>
2022-06-11 10:26:36 +0530

    Finished Completing Banner Section By Using Datas To The Banner Section Are Fetched With The API And Shown Here.Axios Is Installed And Implemented And Imported Here.Constants Are Also Created Along With It.

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import './Banner.css' 
+import React , {useEffect, useState} from 'react'
+import {API_KEY,imageUrl} from '../../constants/constants'
+import axios from '../../axios.js'
+import './Banner.css' ;
 
 function Banner() {
+ const [movie,setMovie]=useState('')
+ function randomNumber(min,max){
+  return Math.floor(Math.random()* (max-min+1)+min)
+ }
+ useEffect(() => {
+    axios.get(`/trending/all/day?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data.results[0])
+      setMovie(response.data.results[randomNumber(20,0)])
+  
+    })
+  }, [])
+  console.log(movie)
+  
+
   return (
-    <div className='banner'>
+    <div 
+    style={{backgroundImage:`url(${movie?imageUrl+movie.backdrop_path:''})`}} className='banner'>
+      
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+            <h1 className='title'> {movie ? movie.title ?movie.title:movie.name:''}</h1>
             <div className='banner_button'>
                 <button className='button'>Play</button>
                 <button className='button'>My List</button>
             </div>
-            <h1 className='description'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the</h1>
+            <h1 className='description'>{movie ? movie.overview:''} </h1>
         </div>
         <div className="fade_bottom"></div>
     </div>
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = "https://api.themoviedb.org/3";
+export const API_KEY = "6a5dbf8c48d865a8fcbd289f6fd3ac6c";
+export const imageUrl= "https://image.tmdb.org/t/p/original";
