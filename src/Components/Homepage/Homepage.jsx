58f489c9151315e4b9e965fe3af0c7eef5130a13
mmmmm1233212344 <abhaymilan8@gmail.com>
2022-11-27 22:04:53 +0530

    Navbar Design

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
+import './Homepage.css'
 
 function Homepage() {
   return (
     <div>
+<header>
+  <nav>
+  <img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" width="200" alt="netflix logo" /> 
+  <section>
 
-      <header>
-        <nav>
-          <img src="" alt="" />
-          <input type="" />
-        </nav>
-      </header>
-    </div>
+ <label for="language">
+  Language
+ </label>
+ <select name="language" id="language">
+  <option value="English">English</option>
+  <option value="English">Spanish</option>
+  <option value="English">Finnish</option>
+  <option value="English">Ireland</option>
+  <option value="English">German</option>
+ </select>
+ <button>
+  Sign Out
+ </button>
+  </section>
+ 
+
+  </nav>
+</header>
+
+
+  </div>
   )
 }
 
-export default Homepage
+export default Homepage;
