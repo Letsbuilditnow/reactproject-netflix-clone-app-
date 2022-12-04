4aa2e054f1c6916624739577b103bc685580cf97
mmmmm1233212344 <abhaymilan8@gmail.com>
2022-12-04 23:05:19 +0530

    Minor Changes

--- src/Components/SignupEmail/SignupEmail.jsx
+++ src/Components/SignupEmail/SignupEmail.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function SignupEmail() {
   return (
     <div>
-        
+        <input type="text" />
+        <button><h2>Get Started > </h2></button>
     </div>
   )
 }
