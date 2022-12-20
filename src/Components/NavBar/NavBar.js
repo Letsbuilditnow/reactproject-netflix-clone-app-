fc930188b1a895bec7bd7f1d680fa4576c2fe594
mmmmm1233212344 <abhaymilan8@gmail.com>
2022-12-20 23:14:51 +0530

    Added Movies To Navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -8,6 +8,7 @@ function NavBar() {
 <ul>
    <li>Home</li>
    <li>TV Shows</li>
+   <li>Movies</li>
 </ul>
 <img className="homeavatar" src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="" />
 
